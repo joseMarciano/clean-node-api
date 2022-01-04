694df2c1c23688f76041a049405bfecd1e8f6e39
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-04 12:19:15 -0300

    test: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/addaccount/dbAddAcount.spec.ts
+++ src/data/usecases/addaccount/dbAddAcount.spec.ts
@@ -1,9 +1,10 @@
-import { Encrypter } from './dbAddAccountProtocols';
+import { AccountModel, AddAccountModel, Encrypter, AddAccountRepository } from './dbAddAccountProtocols';
 import { DbAddAccount } from './DbAddAccount';
 
 interface SutTypes {
   sut: DbAddAccount
   encrypterStub: Encrypter
+  addAccountRepositoryStub: AddAccountRepository
 }
 
 const makeEncrypter = (): Encrypter => {
@@ -16,13 +17,32 @@ const makeEncrypter = (): Encrypter => {
   return new EncrypterStub();
 }
 
+const makeAddAccountRepository = (): AddAccountRepository => {
+  class AddAccountRepositoryStub implements AddAccountRepository {
+    async add (accountModel: AddAccountModel): Promise<AccountModel> {
+      const fakeAccount = {
+        id: 'valid_id',
+        name: 'valid_name',
+        email: 'valid_email@mail.com',
+        password: 'hashed_password'
+      }
+
+      return Promise.resolve(fakeAccount)
+    }
+  }
+
+  return new AddAccountRepositoryStub();
+}
+
 const makeSut = (): SutTypes => {
   const encrypterStub = makeEncrypter();
-  const sut = new DbAddAccount(encrypterStub);
+  const addAccountRepositoryStub = makeAddAccountRepository();
+  const sut = new DbAddAccount(encrypterStub, addAccountRepositoryStub);
 
   return {
     sut,
-    encrypterStub
+    encrypterStub,
+    addAccountRepositoryStub
   }
 };
 
@@ -57,5 +77,24 @@ describe('DbAddAccount use case', () => {
     const promise = sut.add(accountData);
 
     await expect(promise).rejects.toThrow();
+  });
+
+  test('Should call AddAccountRepository with correct values', async () => {
+    const { sut, addAccountRepositoryStub } = makeSut();
+
+    const addAccountRepositoryStubSpy = jest.spyOn(addAccountRepositoryStub, 'add');
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email@mail.com',
+      password: 'valid_password'
+    }
+
+    await sut.add(accountData);
+
+    expect(addAccountRepositoryStubSpy).toBeCalledWith({
+      ...accountData,
+      password: 'hashed_password'
+    });
   });
 });
