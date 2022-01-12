96f5191a67f3d47497e6586bd12f9a00eb8905b8
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-11 22:21:31 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/bodyparser.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import app from '../config/app';
+
+describe('Body Parser Middleware', () => {
+  test('Should parse body as json', async () => {
+    app.post('/test_body_parser', (req, res) => {
+      return res.send(req.body);
+    });
+
+    await request(app)
+      .post('/test_body_parser')
+      .send({ name: 'José Paulo' })
+      .expect({ name: 'José Paulo' })
+  })
+});
