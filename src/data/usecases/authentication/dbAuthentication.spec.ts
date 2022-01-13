4573cccd2d1ffdbc1cc4463883ad826cb7d7bfb1
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-13 11:13:22 -0300

    test: ensure DbAuthentication throws if LoadAccountByEmailRepository throws

--- src/data/usecases/authentication/dbAuthentication.spec.ts
+++ src/data/usecases/authentication/dbAuthentication.spec.ts
@@ -47,5 +47,13 @@ describe('DbAuthentication', () => {
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load');
     await sut.auth(makeFakeAuthenticaction());
     expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com');
-  })
+  });
+
+  test('Should throw if LoadAccountByEmailRepository throws', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut();
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(Promise.reject(new Error()));
+
+    const promise = sut.auth(makeFakeAuthenticaction());
+    await expect(promise).rejects.toThrow();
+  });
 });
