a9e43b33c1083476969ef2d8cce395ce786a91f0
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-14 14:21:25 -0300

    refactor: rename SignUpRoutes with LoginRoutes

--- src/main/routes/loginRoutes.test.ts
+++ src/main/routes/loginRoutes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongoHelper';
 import app from '../config/app';
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL)
   });
@@ -15,15 +15,17 @@ describe('SignUp Routes', () => {
     await MongoHelper.disconnect();
   })
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'José Paulo Zanardo Marciano',
-        email: 'marcianojosepaulo@outlook.com',
-        password: '123456789',
-        passwordConfirmation: '123456789'
-      })
-      .expect(200)
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'José Paulo Zanardo Marciano',
+          email: 'marcianojosepaulo@outlook.com',
+          password: '123456789',
+          passwordConfirmation: '123456789'
+        })
+        .expect(200)
+    });
   });
 });
