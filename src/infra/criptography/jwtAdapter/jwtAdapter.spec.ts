90038210d4eebd8efeecccef832204b2dba11ade
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-13 22:00:39 -0300

    refactor: move sut creation to a factory method

--- src/infra/criptography/jwtAdapter/jwtAdapter.spec.ts
+++ src/infra/criptography/jwtAdapter/jwtAdapter.spec.ts
@@ -1,17 +1,32 @@
 import { JwtAdapter } from './JwtAdapter';
 
 import jwt from 'jsonwebtoken';
+import { Encrypter } from '../../../data/protocols/criptography/Encrypter';
+
+interface SutTypes {
+  sut: Encrypter
+  SECRET: string
+}
+
+const makeSut = (): SutTypes => {
+  const SECRET = 'secret'
+  const sut = new JwtAdapter(SECRET);
+
+  return {
+    sut,
+    SECRET
+  }
+}
 
 jest.mock('jsonwebtoken', () => ({
   async sign (): Promise<string> {
     return Promise.resolve('any_token');
   }
-}))
+}));
 
 describe('Jwt Adapter', () => {
   test('Should call sign with correct values', async () => {
-    const SECRET = 'secret'
-    const sut = new JwtAdapter(SECRET);
+    const { sut, SECRET } = makeSut();
 
     const signSpy = jest.spyOn(jwt, 'sign');
 
@@ -21,8 +36,7 @@ describe('Jwt Adapter', () => {
   });
 
   test('Should return a token on sign success', async () => {
-    const SECRET = 'secret'
-    const sut = new JwtAdapter(SECRET);
+    const { sut } = makeSut();
 
     const accessToken = await sut.encrypt('any_id');
 
@@ -30,8 +44,7 @@ describe('Jwt Adapter', () => {
   });
 
   test('Should throw if sign throws', async () => {
-    const SECRET = 'secret'
-    const sut = new JwtAdapter(SECRET);
+    const { sut } = makeSut();
 
     jest.spyOn(jwt, 'sign').mockImplementationOnce(() => { throw new Error() });
 
