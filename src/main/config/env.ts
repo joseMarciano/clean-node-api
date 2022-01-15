744b90dee812a707010751d3ac4839a7522409ad
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-14 22:10:23 -0300

    refactor: add environment variable to be able select the correct routes directories

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,12 @@
+import path from 'path';
+
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
-  jwtSecret: process.env.JWT_SECRET || '/-*/@(*&!@#@&!#}kaj @@'
+  jwtSecret: process.env.JWT_SECRET || '/-*/@(*&!@#@&!#}kaj @@',
+  routesDir: (): string => {
+    let extensionFile = '.ts';
+    if (process.env.NODE_ENV === 'production') extensionFile = '.js';
+    return `${path.resolve(__dirname, '../routes')}/**Routes${extensionFile}`;
+  }
 }
--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,15 +1,15 @@
 import { Router, Express } from 'express';
 import fg from 'fast-glob';
+import env from './env';
 
 export default (app: Express): void => {
   const router = Router();
 
   app.use('/api', router);
 
-  const fileNames = fg.sync('**/src/main/routes/**Routes.ts');
-
+  const fileNames = fg.sync(env.routesDir());
   fileNames.map(async file => {
-    const route = (await import(`../../../${file}`)).default;
-    route(router)
+    const route = (await import(file)).default;
+    route(router);
   })
 };
