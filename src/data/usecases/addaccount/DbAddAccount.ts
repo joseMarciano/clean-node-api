c23147ca555f3e215702b7f1180894e30a0368ea
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-17 14:50:06 -0300

    feat: ensure DbAddAccount calls LoadAccountByEmailRepository with correct email

--- src/data/usecases/addaccount/DbAddAccount.ts
+++ src/data/usecases/addaccount/DbAddAccount.ts
@@ -1,12 +1,22 @@
-import { AddAccount, AddAccountModel, Hasher, AccountModel, AddAccountRepository } from './dbAddAccountProtocols';
+import {
+  AddAccount,
+  AddAccountModel,
+  Hasher,
+  AccountModel,
+  AddAccountRepository,
+  LoadAccountByEmailRepository
+} from './dbAddAccountProtocols';
 
 export class DbAddAccount implements AddAccount {
   constructor (
     private readonly hasher: Hasher,
-    private readonly addAccountRepository: AddAccountRepository
+    private readonly addAccountRepository: AddAccountRepository,
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
   ) {}
 
   async add (accountModel: AddAccountModel): Promise<AccountModel> {
+    await this.loadAccountByEmailRepository.loadByEmail(accountModel.email);
+
     const hashedPassword = await this.hasher.hash(accountModel.password);
 
     const account = await this.addAccountRepository.add({
--- src/data/usecases/addaccount/dbAddAccountProtocols.ts
+++ src/data/usecases/addaccount/dbAddAccountProtocols.ts
@@ -2,3 +2,4 @@ export * from '../../../domain/usecases/AddAccount';
 export * from '../../../domain/model/Account';
 export * from '../../protocols/criptography/Hasher';
 export * from '../../protocols/db/account/AddAccountRepository';
+export * from '../../protocols/db/account/LoadAccountByEmailRepository'
