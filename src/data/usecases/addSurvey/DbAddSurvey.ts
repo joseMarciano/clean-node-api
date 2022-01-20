989a845f799746026e31c2c4e68a881369654e49
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-19 23:05:57 -0300

    feat: ensure DbAddSurvey class AddSurveyRepository with correct values

---
+++ src/data/usecases/addSurvey/DbAddSurvey.ts
@@ -0,0 +1,15 @@
+import {
+  AddSurveyRepository,
+  AddSurvey,
+  AddSurveyModel
+} from './dbAddSurveyProtocols';
+
+export class DbAddSurvey implements AddSurvey {
+  constructor (
+    private readonly addSurveyRepository: AddSurveyRepository
+  ) {}
+
+  async add (data: AddSurveyModel): Promise<void> {
+    await this.addSurveyRepository.add(data);
+  }
+}
