8c3749172c30c64eb04faa3b8b15dfb9f56490e4
joseMarciano <marcianojosepaulo@gmail.com>
2022-01-25 21:31:40 -0300

    test: ensure AuthController calls LoadAccountByToken with correct token

--- src/presentation/controllers/auth/authController.spec.ts
+++ src/presentation/controllers/auth/authController.spec.ts
@@ -1,16 +1,39 @@
+import { LoadAccountByToken } from '../../../domain/usecases/LoadAccountByToken';
 import { AccessDeniedError } from '../../errors';
 import { forbidden } from '../../helpers/httpHelper';
+import { HttpRequest } from '../../protocols';
 import { Auth } from '../../protocols/Auth'
+import { AccountModel } from '../login/signup/signupProtocols';
 import { AuthController } from './AuthController';
+
+const makeFakeAccount = (): AccountModel => ({
+  id: 'any_id',
+  email: 'any_email',
+  name: 'any_name',
+  password: 'any_password'
+})
+
+const makeLoadAccountByToken = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    async load (accessToken: string, role?: string): Promise<AccountModel> {
+      return Promise.resolve(makeFakeAccount())
+    }
+  }
+
+  return new LoadAccountByTokenStub()
+}
 
 interface SutTypes {
   sut: Auth
+  loadAccountByTokenStub: LoadAccountByToken
 }
 const makeSut = (): SutTypes => {
-  const sut = new AuthController();
+  const loadAccountByTokenStub = makeLoadAccountByToken()
+  const sut = new AuthController(loadAccountByTokenStub);
 
   return {
-    sut
+    sut,
+    loadAccountByTokenStub
   }
 }
 
@@ -21,5 +44,21 @@ describe('AuthController', () => {
     const httpResponse = await sut.handle({})
 
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
+
+  test('Should return call LoadAccountByToken with correct access-token', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+
+    const httpRequest: HttpRequest = {
+      headers: {
+        'x-access-token': 'any_token'
+      }
+    }
+
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, 'load')
+
+    await sut.handle(httpRequest)
+
+    expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
 })
